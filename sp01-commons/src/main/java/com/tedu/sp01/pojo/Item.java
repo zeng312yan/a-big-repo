fc7ef9862c8c3bb20bf4673f39f24f86af1ea2e3
Tarena <Tarena@Tarena-PC>
2019-06-28 10:24:30 +0800

    添加注释

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -9,8 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
+	//商品id
 	private Integer id;
+	//商品名称
 	private String name;
+	//商品数量
 	private Integer number;
 	
 	//1111111111111
